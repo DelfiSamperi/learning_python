9519c74a6225528f5bb28435c7a15c8a656e6230
Maria Delfina Samperi <delfisamperi@gmail.com>
2026-05-10 00:08:47 -0300

    new projects

---
+++ adivina_el_numero.py
@@ -0,0 +1,1 @@
+def adivina_el_numero(x)
--- historias_locas.py
+++ historias_locas.py
@@ -2,7 +2,19 @@
 # Queremos crear una cadena que diga:
 # Aprende a programar con _______ .
 
-organizacion = "freeCodeCamp"
+# organizacion = "freeCodeCamp"
 
-print("Aprende a programas con " + organizacion)
-print("Aprende a programar con {}".format(organizacion))
+# print("Aprende a programas con " + organizacion)
+# print("Aprende a programar con {}".format(organizacion))
+# print(f"Aprende a programar con {organizacion}") # f-string
+
+# Mad Libs (Historias Locas)
+
+adj = input("Ingresa un adjetivo: ")
+verbo1 = input("Ingresa un verbo: ")
+verbo2 = input("Ingresa un segundo verbo: ")
+sustantivo_plural = input("Ingresa un sustantivo plural: ")
+
+madlib = f"Programar es tan {adj}! Siempre me emociona porque me encanta {verbo1} problemas. Aprende a {verbo2} con freeCodeCamp y alcanza tus {sustantivo_plural}."
+
+print(madlib)
